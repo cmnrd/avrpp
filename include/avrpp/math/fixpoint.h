f5dfc595ace1d13205ceefd16c6032226ebf3618
chrism333 <christian.menard@gmx.de>
2014-03-24 17:41:22 +0100

    bug fix

--- include/avrpp/math/fixpoint.h
+++ include/avrpp/math/fixpoint.h
@@ -425,8 +425,8 @@ namespace avrpp
 			}
 		}
 		
-		static double toDouble() { return ((double) (this->fixpoint)) / ( ( (T) 1) << fractionDigits); }
-		static float  toFloat()  { return ((float) (this->fixpoint)) / ( ( (T) 1) << fractionDigits); }
+		double toDouble() { return ((double) (this->fixpoint)) / ( ( (T) 1) << fractionDigits); }
+		float  toFloat()  { return ((float) (this->fixpoint)) / ( ( (T) 1) << fractionDigits); }
 
 		template<typename U>
 		static Fixpoint arctan2( U a, U b)
